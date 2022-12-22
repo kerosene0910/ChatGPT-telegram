fe20226d5909238592f880eb20704674460cbbc3
rafalohaki <rafalohaki@gmail.com>
2022-12-22 23:13:44 +0100

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -12,7 +12,7 @@ def chatgpt_response(prompt):
         max_tokens=1024,
         n=1,
        stop=None,
-        temperature=0.7,
+        temperature=0.8,
     )
 
     message = completions.choices[0].text
